7d981a79fb08dd836b95364ee095844b6eda7107
Kostya <generali8@mail.ru>
2025-01-14 16:16:51 +0300

    Update Comments.jsx

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import c from "./Comments.module.css";
+import styles from "./Comments.module.css";
 import { useEffect, useState } from "react";
 import { getCommentsData } from "../../utils/Api";
 
@@ -16,13 +16,13 @@ function Comments() {
   if (comments.length == 0) return <></>;
 
   return (
-    <div className={c.comments}>
+    <div className={styles.comments}>
       <div>
         <span>{comments.user?.username}</span>
         <span>{` (${comments.user?.fullName}): `}</span>
         <span>{comments.body}</span>
       </div>
-      <div className={c.badge}>
+      <div className={styles.badge}>
         <img
           src="https://img.icons8.com/?size=100&id=24816&format=png"
           alt="like icon"
